8348d0a24f403e34dbc1e9db2128b2647e6593dc
angelol-git <ang.ladores.dev@gmail.com>
2025-12-15 12:57:00 -0500

    updates to newchat

--- client/src/pages/chat/NewChat.jsx
+++ client/src/pages/chat/NewChat.jsx
@@ -33,31 +33,26 @@ function NewChat() {
   }
 
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full h-full items-center flex flex-col">
       <ChatHeader
         isSideBarOpen={isSideBarOpen}
         setIsSideBarOpen={setIsSideBarOpen}
         isMobile={isMobile}
       />
-      <div className="items-center flex flex-col justify-center flex-1 w-full lg:min-h-0 ">
-        <div className="relative max-w-screen-xl flex flex-col flex-1 py-2 px-4 w-full h-full">
-          <div className="flex flex-col gap-4 h-full py-2 items-center justify-center pb-50 text-center">
-            <h2 className="text-primary text-2xl font-medium font-lora pb-5">
-              What recipe can I help you with?
-            </h2>
-            <div className="text-secondary">
-              Paste a link to any recipe, and I’ll extract the ingredients and
-              steps.
-            </div>
-            <div className="text-secondary">
-              Ask me to improve a recipe — healthier, quicker, or more
-              flavorful.
-            </div>
-            <div className="text-secondary">
-              Ask to double, halve, or scale the recipe for any number of
-              servings.
-            </div>
-          </div>
+
+      <div className="text-center relative gap-4 p-6 max-w-screen-xl flex flex-col w-full h-full justify-center pb-50">
+        <h2 className="text-primary text-2xl font-medium font-lora pb-5">
+          What recipe can I help you with?
+        </h2>
+        <div className="text-secondary">
+          Paste a link to any recipe, and I’ll extract the ingredients and
+          steps.
+        </div>
+        <div className="text-secondary">
+          Ask me to improve a recipe — healthier, quicker, or more flavorful.
+        </div>
+        <div className="text-secondary">
+          Ask to double, halve, or scale the recipe for any number of servings.
         </div>
         {/* <ChatErrorModal
               isErrorModalOpen={isErrorModalOpen}
